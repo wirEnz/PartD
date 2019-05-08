8df44f45e989ffdae5730111ba4fe12e1ae95f2f
wirEnz <fps.andrew@gmail.com>
2019-05-08 17:53:23 +1200

    adding day.

--- src/PartD.java
+++ src/PartD.java
@@ -6,6 +6,7 @@ public class PartD {
 		System.out.println("Hi, my name is Andrew");
 		System.out.println("My favourite colour is Blue");
 		System.out.println("My favourite mode of transportation is walking");
+		System.out.println("The best day of the week is Friday");
 		
 
 	}
