9dce1947a0fe846ee3a1347f1b93ea0e257a657a
wirEnz <fps.andrew@gmail.com>
2019-05-08 17:52:37 +1200

    adding transportation

--- src/PartD.java
+++ src/PartD.java
@@ -5,6 +5,7 @@ public class PartD {
 		
 		System.out.println("Hi, my name is Andrew");
 		System.out.println("My favourite colour is Blue");
+		System.out.println("My favourite mode of transportation is walking");
 		
 
 	}
